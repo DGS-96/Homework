a628567aa2fb182c673b3c373a43fd899705d2ee
Daniil-Gorchakov <usernam33@yandex.ru>
2021-10-02 19:46:16 +0300

    first task

---
+++ Daniil-Gorchakov/Task 6-1.py
@@ -0,0 +1,48 @@
+class Counter():
+	"""docstring for Counter"""
+
+	def __init__(self, start=0,  stop=-1):
+		self.present_value = start
+		self.stop = stop
+
+	def increment(self):
+		if self.present_value == self.stop:
+			#print("Maximal value is reached.")
+			raise Exception("Maximal value is reached.")
+		else:
+			self.present_value += 1
+
+	def get(self):
+		#print(f"{self.present_value = }")
+		print(self.present_value)
+		
+if __name__ == "__main__":
+	c = Counter(start=42)
+	c.increment()
+	c.get()
+	#>>> 43
+	print()
+
+	c = Counter()
+	c.increment()
+	c.get()
+	#>>> 1
+	c.increment()
+	c.get()
+	#>>> 2
+	print()
+
+	c = Counter(start=42, stop=43)
+	c.increment()
+	c.get()
+	#>> 43
+	c.increment()
+	#>>> Maximal value is reached.
+	c.get()
+	#>>> 43
+	print()
+
+	test = Counter()
+	for _ in range(10):
+		test.increment()
+	test.get()
